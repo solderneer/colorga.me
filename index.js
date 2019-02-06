220d0fefb2793444be668613e78ddc2cf1f17446
Sudharshan <solderneer@gmail.com>
2019-02-06 22:28:34 +0800

    hue rotate support

--- index.js
+++ index.js
@@ -2,4 +2,32 @@
 import 'normalize.css'
 import './main.css'
 
-console.log(Date());
+// Logic imports
+import randomMC from 'random-material-color'
+
+let randArray = ['one', 'two', 'three']
+
+function setColor() {
+  let newColor = randomMC.getColor()
+
+  // Setting to random color
+  document.querySelector('.container').style.backgroundColor = newColor
+  
+  for (const elem of document.querySelectorAll('.option')) {
+    elem.style.backgroundColor = newColor
+  }
+
+  let wrongOptionOne = Math.floor(Math.random() * 3)
+  let wrongOptionTwo = Math.floor(Math.random() * 3)
+  
+  while (wrongOptionOne === wrongOptionTwo)
+    wrongOptionTwo = Math.floor((Math.random() * 3) + 1)
+
+  document.querySelector(`#${randArray[wrongOptionOne]}`).style.filter = "hue-rotate(10deg)"
+  document.querySelector(`#${randArray[wrongOptionTwo]}`).style.filter = "hue-rotate(-10deg)"
+
+
+  return randArray[(3 - wrongOptionOne - wrongOptionTwo)]
+}
+
+console.log(setColor())
